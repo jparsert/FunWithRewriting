c8ab5ca2e1805d1a0ff57f48b500d80bc3843c83
Julian Parsert <julian.parsert@student.uibk.ac.at>
2017-03-19 15:48:59 +0100

    fixed matching algorigthm bug

--- src/Main.ml
+++ src/Main.ml
@@ -11,12 +11,17 @@ let _ = print_string @@ (Matching.string_of_matching @@
                         Matching.matching (Fun ("f", [Var "X" ; Var "Y"])) (Fun ("f", [Fun ("a",[]);Fun ("b",[])])))^"\n"
 let _ = print_string @@ (Matching.string_of_matching @@ Matching.matching (Fun ("f", [Var "A"])) (Fun ("g", [Var "C"])))^"\n"
 
+let _ = let t1 = Fun ("+" , [Var "x" ; Fun ("s" , [Fun ("+",[Var "y" ; Var "z"])])]) in
+  let t2 = Fun ("+" ,[Fun ("s", [Var "y"]) ; Fun ("s", [Fun ("+" , [Fun ("+", [Var "x";Fun ("s", [Fun ("0",[])])]) ; Var "z"])])]) in
+  print_string @@ (Matching.string_of_matching @@  Matching.matching t1 t2)^"\n"
+
 let _ = let s = [("x", Var "a");("y", Fun ("f",[Var "x"]))] in
   let t = Fun ("g" ,[ Var "a" ; Var "x" ; Var "y"]) in
   let subst = apply_substitutions s t in
   print_string @@ (string_of_substitution_list s)^"\n";
   print_string @@ (string_of_term t)^"\n";
   print_string @@ (Term.string_of_term subst)^"\n"
+
 (*
 let _ =
   let ter = Fun ("f" , [Var "x"]) in
--- src/Matching.ml
+++ src/Matching.ml
@@ -9,15 +9,21 @@ type result = Bottom | Solution of Substitution.t list
 let rec matching s t =
   let rec sol_union acc = function
     | [] -> Solution acc
-    | (Solution x)::xs -> sol_union (x@acc) xs
+    | (Solution x)::xs -> sol_union(x@acc) xs
     | Bottom::xs -> Bottom
+  in
+  let rec unpack_option acc = function
+    | None -> Bottom
+    | Some x -> sol_union [] x
   in
   match (s, t) with
   | (Var v1, Var v2) -> Solution [(v1, Var v2)]
   | (Var v, Fun f) -> Solution [(v, Fun f)]
   | (Fun f, Var v) -> Bottom
-  | (Fun (f, _ ) , Fun (g,_)) when f<>g -> Bottom
-  | (Fun (f, args1) , Fun(g, args2)) -> sol_union []  @@ List.map2 (matching) args1 args2
+  | (Fun (f, _) , Fun (g,_)) when f<>g -> Bottom
+  | (Fun (f, args1) , Fun(g, args2)) -> unpack_option []
+                                          (try Some (List.map2 (matching) args1 args2) with
+                                          _ -> None)
 
 let rec string_of_matching = function
   | Bottom -> Utils.bottom_symbol
